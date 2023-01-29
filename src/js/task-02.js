76c2d62b66befed76ee0bc5d6508642d68f7edeb
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-01-29 13:10:15 +0200

    Adding the solution to the second task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsContainerEl = document.querySelector('#ingredients');
+
+const createIngredientsItems = (items) => {
+  return items.map(ingredient => {
+    const item = document.createElement('li');
+    item.classList.add('item');
+    item.textContent = ingredient;
+  
+    return item;
+  });
+};
+
+const itemsEl = createIngredientsItems(ingredients);
+
+ingredientsContainerEl.append(...itemsEl);
+console.log(ingredientsContainerEl);
