c9b819552a122657307520840dfdda495336c713
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-02-04 17:00:07 +0200

    adds a solution to the eight task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit (submit) {
+    submit.preventDefault();
+
+    const {elements: { email, password }
+  } = submit.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+    return alert('Будьласка заповніть всі поля!');
+  }
+
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+    submit.currentTarget.reset();
+}
