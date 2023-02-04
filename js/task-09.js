0ebf3137a786fedaa5bb94361e76657d42a593c4
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-02-04 17:00:40 +0200

    adds a solution to the nine task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+const bodyEl = document.querySelector('body');
+const colorName = document.querySelector('.color');
+const changeColorBtn =document.querySelector('.change-color');
+
+console.dir(bodyEl);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor () {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = getRandomHexColor();
 }
