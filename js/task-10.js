ce33fa757138759ee49e0dbc5bd555696cb1f849
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-02-04 21:51:54 +0200

    adds a solution to the tenth task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector('input')
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', createDiv);
+
+function createDiv(amount) {
+  amount = inputEl.value;
+  const boxesArr = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement('div');
+    div.style.width = 30 + i*10 + 'px';
+    div.style.height = 30 + i*10 + 'px';
+    div.style.backgroundColor = getRandomHexColor();
+    
+    boxesArr.push(div);
+
+    boxesEl.append(...boxesArr);
+  }
+}
+
+destroyBtn.addEventListener('click', destroyDiv)
+
+function destroyDiv() {
+  boxesEl.innerHTML = '';
+}
